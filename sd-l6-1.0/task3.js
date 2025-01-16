e6cd55b3abba87f2883ac562a322a6a6cebd6455
Ana <anaforero91@gmail.com>
2025-01-15 22:40:15 -0500

    Todos los test pasan

--- sd-l6-1.0/task3.js
+++ sd-l6-1.0/task3.js
@@ -1,22 +1,45 @@
 // Task 3: addUser(first_name, last_name, email)
-import fetch from 'node-fetch'; 
+import fetch from 'node-fetch';
 
 export function addUser(first_name, last_name, email) {
-    fetch("http://localhost:3000/users", {
-        method: "POST",
-        body: JSON.stringify({
-                     
-            first_name: first_name,
-            last_name: last_name,
-            email: email
-          }),
-        
-        headers: {
-          "Content-Type": "application/json; charset=UTF-8"
-        }
-      })
-        .then(response => response.json())  
-        .then(users => console.log("Lista de usuarios:", users))  
+
+    // Se obtiene la info del servidor
+    fetch("http://localhost:3000/users")
+        .then(response => response.json())
+        .then(users => {
+  
+            let lastId = 0;
+            let idList = []; 
+
+            //Se agregan los id a un arreglo convertidos a número
+            for (const user of users) {
+              idList.push(Number(user.id)); 
+            }
+
+            //Se identifica el último id y se genera el nuevo id
+            lastId = Math.max(...idList);         
+            const newId = lastId + 1;
+            const newIdStr = String(newId);
+            
+
+            //Se agrega con el método HTTP, "POST"      
+            fetch("http://localhost:3000/users", {
+                method: "POST",
+                body: JSON.stringify({
+                    id: newIdStr, 
+                    first_name: first_name,
+                    last_name: last_name,
+                    email: email
+                }),
+                headers: {
+                    "Content-Type": "application/json; charset=UTF-8"
+                }
+            })
+                .then(response => response.json())
+                .then(user => console.log("Nuevo usuario agregado:", user))
+        })    
 }
 
-addUser("nina", "forero", "cf@email.com");
+
+
+
--- sd-l6-1.0/task4.js
+++ sd-l6-1.0/task4.js
@@ -1,2 +1,14 @@
 // Task 4: delUser(number)
+import fetch from 'node-fetch';
 
+export function delUser(id) {
+    fetch(`http://localhost:3000/users/${id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json; charset=UTF-8"
+        }
+    })
+        .then(response => response.json())
+        .then(data => console.log("Usuario borrado:", data))
+        
+}
